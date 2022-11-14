3388531f91ff4d50872056eb3aac36cc30e5ce26
Nay45 <nayottama45@gmail.com>
2022-11-14 09:46:39 +0700

    Back Button

--- app/src/main/java/com/example/intent/MoveActivity.kt
+++ app/src/main/java/com/example/intent/MoveActivity.kt
@@ -1,11 +1,26 @@
 package com.example.intent
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
-class MoveActivity : AppCompatActivity() {
+class MoveActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_move)
+
+        val btnBack : Button = findViewById(R.id.btn_back)
+        btnBack.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.btn_back -> {
+                val backToMenu = Intent(this@MoveActivity, MainActivity :: class.java)
+                startActivity(backToMenu)
+            }
+        }
     }
 }
